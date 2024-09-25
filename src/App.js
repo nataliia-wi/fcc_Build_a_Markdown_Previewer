492d08f20a20c1799130b26b820900afccacbb89
Nataliia S <natalilu1990@gmail.com>
2024-09-26 01:16:36 +0300

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+![picture](https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg)
+
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+`
 
 function App() {
+  const [markdownText, setMarkdownText] = useState(defaultMarkdown)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ textAlign: 'center' }}>Markdowm Previewer</h1>
+      <div className='boxes-container'>
+        <textarea name="editor" id="editor" value={markdownText} onChange={(e) => setMarkdownText(e.target.value)}></textarea>
+        <div id="preview">
+          <ReactMarkdown>{markdownText}</ReactMarkdown>
+        </div>
+
+      </div>
     </div>
   );
 }
